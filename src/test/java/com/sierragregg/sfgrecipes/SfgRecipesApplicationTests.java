141e8a35d917255b41fdaf24d7a22f1ca551046f
LifelongLearner13 <sierragregg@gmail.com>
2020-04-02 12:08:40 -0500

    initial commit

---
+++ src/test/java/com/sierragregg/sfgrecipes/SfgRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sierragregg.sfgrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
